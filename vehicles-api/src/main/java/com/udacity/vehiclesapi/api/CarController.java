0b03f5d04fe2a8154fee821db91aa5b9897494ca
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-10-01 14:40:36 -0300

    Added api, had to downgrade spring release

---
+++ vehicles-api/src/main/java/com/udacity/vehiclesapi/api/CarController.java
@@ -0,0 +1,57 @@
+package com.udacity.vehiclesapi.api;
+
+import com.udacity.vehiclesapi.domain.car.Car;
+import com.udacity.vehiclesapi.service.CarService;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping("/cars")
+public class CarController {
+
+    private final CarService carService;
+    private final CarResourceAssembler assembler;
+
+    public CarController(CarService carService, CarResourceAssembler assembler) {
+        this.carService = carService;
+        this.assembler = assembler;
+    }
+
+    /**
+     * Creates a list to store any vehicles.
+     * @return list of vehicles
+     */
+    @GetMapping
+    Resources<Resource<Car>> list(){
+        List<Resource<Car>> resources = carService.list().stream().map(assembler::toResource)
+                .collect(Collectors.toList());
+        return new Resources<>(resources,
+                linkTo(methodOn(CarController.class).list()).withSelfRel());
+
+    }
+
+    /**
+     * Gets information of a specific car by ID.
+     * @param id the id number of the given vehicle
+     * @return all information for the requested vehicle
+     */
+    @GetMapping("/{id}")
+    Resource<Car> get(@PathVariable Long id) {
+        /**
+         * TODO: Use the `findById` method from the Car Service to get car information.
+         * TODO: Use the `assembler` on that car and return the resulting output.
+         *   Update the first line as part of the above implementing.
+         */
+        return assembler.toResource(new Car());
+    }
+}
