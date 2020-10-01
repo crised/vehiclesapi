da16f77e1fd353a5be5a5c7396e6270e00e1d13c
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-10-01 11:03:10 -0300

    Added Car/Manufacturer entities and repositories

---
+++ vehicles-api/src/main/java/com/udacity/vehiclesapi/service/CarService.java
@@ -0,0 +1,14 @@
+package com.udacity.vehiclesapi.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Implements the car service create, read, update or delete
+ * information about vehicles, as well as gather related
+ * location and price data when desired.
+ */
+@Service
+public class CarService {
+
+
+}
